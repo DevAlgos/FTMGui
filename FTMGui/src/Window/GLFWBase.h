42cc60e01c1338728812dd22ab103f834cc4b018
khalid fayz <alanfayz123@gmail.com>
2024-02-03 21:38:58 +0000

    mac fix

--- FTMGui/src/Window/GLFWBase.h
+++ FTMGui/src/Window/GLFWBase.h
@@ -1,7 +1,15 @@
 #pragma once
 
+#ifdef VK_MAC
+#define VK_USE_PLATFORM_MACOS_MVK
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#define GLFW_EXPOSE_NATIVE_COCOA
+#include <GLFW/glfw3native.h>
+#else 
 #define VK_USE_PLATFORM_WIN32_KHR
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include <GLFW/glfw3native.h>
+#endif
